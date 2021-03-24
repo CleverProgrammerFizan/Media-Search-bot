b929f564842a65601a6b4750d384004fde091349
Sh-Jil <65016071+Sh-Jil@users.noreply.github.com>
2021-03-24 18:48:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,34 +1,31 @@
 import re
 from os import environ
 
-# Bot information
-SESSION = environ.get('SESSION', 'Media_search')
-USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 1479154
+API_HASH = '6b21cb22818e09132fb904705c31d3a1'
+BOT_TOKEN = '1734210572:AAHv4jY89BZVOQ-7_MBNZekQ5XQc3QsVMoA'
 
 # Bot settings
-MAX_RESULTS = int(environ.get('MAX_RESULTS', 10))
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+MAX_RESULTS = 10
+CACHE_TIME = 300
+USE_CAPTION_FILTER = True
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if re.search('^.\d+$', ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if re.search('^\d+$', user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+ADMINS = [727037917, 'shijilraj']
+CHANNELS = [-1001280193509, -1001370007665, -1001433161521, -1001457070859, -1001332267186, -1001332358174, -1001171274679, 'channelusername']
+AUTH_USERS = []
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://Shijilraj:9747355576@cluster0.qwtjc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = 'Media Search'
+COLLECTION_NAME = 'channel_files'  # If you are using the same database, then use different collection name for each bot
 
 # Messages
 START_MSG = """
 **Hi, I'm Media Search bot by @cinemapranthanmaar**
-
-Here you can search files in inline mode. Just press following buttons and start searching.
+Here you can search files in inline mode. Just press follwing buttons and start searching.
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
